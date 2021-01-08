a8ae0e5f06635931e8338cede6d84b0d07da0195
nragusa <nragusa@gmail.com>
2021-01-08 10:33:33 -0500

    Initial commit

--- cdk_ecr/cdk_ecr_stack.py
+++ cdk_ecr/cdk_ecr_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_ecr as ecr
 
 
 class CdkEcrStack(core.Stack):
@@ -6,4 +7,9 @@ class CdkEcrStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        repo = ecr.Repository(self, 'MyRepository', repository_name='cdk-repo')
+        repo.add_lifecycle_rule(
+            description='Lifecycle rule for cdk-repo',
+            max_image_count=50,
+            rule_priority=1
+        )
